223769db54631c29e33301b3836aebe545dd298f
pshnarola <psh@narola.email>
2021-08-18 16:41:19 +0530

    -enable toggle button to active restaurant status

--- src/redux/actions/manageRestaurantAction.js
+++ src/redux/actions/manageRestaurantAction.js
@@ -1,73 +1,75 @@
-import Axios from './axios';
-import {setAlert} from './alertAction';
-import { logoutUser } from './generalActions';
-import history from '../../history'
+import Axios from "./axios";
+import axios from "axios";
+import { setAlert } from "./alertAction";
+import { logoutUser } from "./generalActions";
+import history from "../../history";
 
-export const getAllRestaurantData=(data)=>{
-    return async(dispatch)=>{
-        try{
-            dispatch({type:"GET_ALLRESTAURANT_REQUEST"});
-            let config= {
-                headers:{
-                 "Content-Type":"application/json"
-                 }
-             }
-            let dataURL=`/super_admin/manage_restaurant/list`
-            let response = await Axios.post(dataURL,JSON.stringify(data),config );
-            dispatch({type:"GET_ALLRESTAURANT_SUCCESS",payload:response.data});
-
+export const getAllRestaurantData = (data) => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: "GET_ALLRESTAURANT_REQUEST" });
+      let config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      let dataURL = `/super_admin/manage_restaurant/list`;
+      let response = await Axios.post(dataURL, JSON.stringify(data), config);
+      dispatch({ type: "GET_ALLRESTAURANT_SUCCESS", payload: response.data });
+    } catch (error) {
+      dispatch({ type: "GET_ALLRESTAURANT_FAILURE", payload: error });
+      if (error.response) {
+        dispatch(setAlert(`${error.response.data.message}`, "danger"));
+        if (error.response && error.response.status == 401) {
+          dispatch(logoutUser(history));
         }
-        catch(error){
-          dispatch({type:"GET_ALLRESTAURANT_FAILURE",payload:error});
-          if (error.response) {
-            dispatch(setAlert(`${error.response.data.message}`, 'danger'));
-            if(error.response&&error.response.status==401){
-              dispatch(logoutUser(history))
-            }
-          } else {
-            dispatch(setAlert('Something Went wrong!', 'danger'));
-          }
-        }
+      } else {
+        dispatch(setAlert("Something Went wrong!", "danger"));
+      }
     }
   };
+};
 
-
-  export const addRestaurantData=(data,perPage,myPage,inputValue)=>{
-    return async(dispatch)=>{
-        try{
-            dispatch({type:"ADD_RESTAURANT_REQUEST"});
-            let config= {
-                headers:{
-                 "Content-Type":"application/json"
-                 }
-             }
-            let dataURL=`/super_admin/manage_restaurant/create`
-            let response = await Axios.post(dataURL,JSON.stringify(data),config );
-            dispatch({type:"ADD_RESTAURANT_SUCCESS",payload:response.data});
-            dispatch(getAllRestaurantData({start:(myPage-1)*perPage,length:perPage,search:inputValue}));
-            dispatch(showAddRestaurantModal(false));
-            dispatch(setAlert('Restaurant Added Successfully .', 'success'));
-
+export const addRestaurantData = (data, perPage, myPage, inputValue) => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: "ADD_RESTAURANT_REQUEST" });
+      let config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      let dataURL = `/super_admin/manage_restaurant/create`;
+      let response = await Axios.post(dataURL, JSON.stringify(data), config);
+      dispatch({ type: "ADD_RESTAURANT_SUCCESS", payload: response.data });
+      dispatch(
+        getAllRestaurantData({
+          start: (myPage - 1) * perPage,
+          length: perPage,
+          search: inputValue,
+        })
+      );
+      dispatch(showAddRestaurantModal(false));
+      dispatch(setAlert("Restaurant Added Successfully .", "success"));
+    } catch (error) {
+      dispatch({ type: "ADD_RESTAURANT_FAILURE", payload: error });
+      if (error.response) {
+        dispatch(setAlert(`${error.response.data.message}`, "danger"));
+        if (error.response && error.response.status == 401) {
+          dispatch(logoutUser(history));
         }
-        catch(error){
-          dispatch({type:"ADD_RESTAURANT_FAILURE",payload:error});
-          if (error.response) {
-            dispatch(setAlert(`${error.response.data.message}`, 'danger'));
-            if(error.response&&error.response.status==401){
-              dispatch(logoutUser(history))
-            }
-          } else {
-            dispatch(setAlert('Something Went wrong!', 'danger'));
-          }
-        }
+      } else {
+        dispatch(setAlert("Something Went wrong!", "danger"));
+      }
     }
   };
+};
 
 //   export const getSelectedCategoryData=(categoryId)=>{
 //     return async(dispatch)=>{
 //         try{
 //             dispatch({type:"GET_SELECTEDCATEGORY_REQUEST"});
-            
+
 //             let response = await Axios.get(`/restaurant_admin/category/${categoryId}`)
 //             dispatch({type:"GET_SELECTEDCATEGORY_SUCCESS",payload:response.data});
 //         }
@@ -77,94 +79,136 @@ export const getAllRestaurantData=(data)=>{
 //     }
 //   };
 
-
-
-  export const updateSelectedRestaurantPassword=(selectedId,data,perPage,myPage,inputValue)=>{
-    return async(dispatch)=>{
-        try{
-            dispatch({type:"UPDATE_RESTAURANT_REQUEST"});
-            let config= {
-                headers:{
-                 "Content-Type":"application/json"
-                 }
-             }
-            let dataURL=`/super_admin/manage_restaurant/update_password/${selectedId}`
-            let response = await Axios.put(dataURL,JSON.stringify(data),config );
-            dispatch({type:"UPDATE_RESTAURANT_SUCCESS",payload:response.data});
-            dispatch(getAllRestaurantData({start:(myPage-1)*perPage,length:perPage,search:inputValue}));
-            dispatch(showUpdateRestaurantModal(false));
-            dispatch(setAlert('Restaurant Updated Successfully .', 'success'));
+export const updateSelectedRestaurantPassword = (
+  selectedId,
+  data,
+  perPage,
+  myPage,
+  inputValue
+) => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: "UPDATE_RESTAURANT_REQUEST" });
+      let config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      let dataURL = `/super_admin/manage_restaurant/update_password/${selectedId}`;
+      let response = await Axios.put(dataURL, JSON.stringify(data), config);
+      dispatch({ type: "UPDATE_RESTAURANT_SUCCESS", payload: response.data });
+      dispatch(
+        getAllRestaurantData({
+          start: (myPage - 1) * perPage,
+          length: perPage,
+          search: inputValue,
+        })
+      );
+      dispatch(showUpdateRestaurantModal(false));
+      dispatch(setAlert("Restaurant Updated Successfully .", "success"));
+    } catch (error) {
+      dispatch({ type: "UPDATE_RESTAURANT_FAILURE", payload: error });
+      if (error.response) {
+        dispatch(setAlert(`${error.response.data.message}`, "danger"));
+        if (error.response && error.response.status == 401) {
+          dispatch(logoutUser(history));
         }
-        catch(error){
-          dispatch({type:"UPDATE_RESTAURANT_FAILURE",payload:error});
-          if (error.response) {
-            dispatch(setAlert(`${error.response.data.message}`, 'danger'));
-            if(error.response&&error.response.status==401){
-              dispatch(logoutUser(history))
-            }
-          } else {
-            dispatch(setAlert('Something Went wrong!', 'danger'));
-          }
-        }
-    }
-  };
-  
-  export const deleteSelectedRestaurantData=(selectedId,perPage,myPage,inputValue)=>{
-    return async(dispatch)=>{
-        try{
-            dispatch({type:"DELETE_RESTAURANT_REQUEST"});
-            let response = await Axios.delete(`/super_admin/manage_restaurant/${selectedId}`)
-            dispatch({type:"DELETE_RESTAURANT_SUCCESS",payload:response.data});
-            dispatch(getAllRestaurantData({start:(myPage-1)*perPage,length:perPage,search:inputValue}));
-            dispatch(showDeleteRestaurantModal(false));
-            dispatch(setAlert('Restaurant Deleted Successfully .', 'warning'));
-        }
-        catch(error){
-            dispatch({type:"DELETE_RESTAURANT_FAILURE",payload:error});
-            if (error.response) {
-              dispatch(setAlert(`${error.response.data.message}`, 'danger'));
-              if(error.response&&error.response.status==401){
-                dispatch(logoutUser(history))
-              }
-            } else {
-              dispatch(setAlert('Something Went wrong!', 'danger'));
-            }
-        }
-    }
-  }
-
-  export const showAddRestaurantModal = (value) => {
-    
-    return async(dispatch)=>{
-      try{
-          await dispatch({type :"SHOW_ADDRESTAURANT_MODAL" , payload :value });
-      }
-      catch(error){
-          console.error(error);
+      } else {
+        dispatch(setAlert("Something Went wrong!", "danger"));
       }
     }
   };
+};
 
-  export const showUpdateRestaurantModal = (value) => {
-    
-    return async(dispatch)=>{
-      try{
-          await dispatch({type :"SHOW_UPDATERESTAURANT_MODAL" , payload :value });
-      }
-      catch(error){
-          console.error(error);
+export const deleteSelectedRestaurantData = (
+  selectedId,
+  perPage,
+  myPage,
+  inputValue
+) => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: "DELETE_RESTAURANT_REQUEST" });
+      let response = await Axios.delete(
+        `/super_admin/manage_restaurant/${selectedId}`
+      );
+      dispatch({ type: "DELETE_RESTAURANT_SUCCESS", payload: response.data });
+      dispatch(
+        getAllRestaurantData({
+          start: (myPage - 1) * perPage,
+          length: perPage,
+          search: inputValue,
+        })
+      );
+      dispatch(showDeleteRestaurantModal(false));
+      dispatch(setAlert("Restaurant Deleted Successfully .", "warning"));
+    } catch (error) {
+      dispatch({ type: "DELETE_RESTAURANT_FAILURE", payload: error });
+      if (error.response) {
+        dispatch(setAlert(`${error.response.data.message}`, "danger"));
+        if (error.response && error.response.status == 401) {
+          dispatch(logoutUser(history));
+        }
+      } else {
+        dispatch(setAlert("Something Went wrong!", "danger"));
       }
     }
   };
+};
 
-  export const showDeleteRestaurantModal = (value) => {
-    
-    return async(dispatch)=>{
-      try{
-          await dispatch({type :"SHOW_DELETERESTAURANT_MODAL" , payload :value });
-      }
-      catch(error){
-          console.error(error);
+export const enableRestaurant = (value) => {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: "ENABLE_RESTAURANT_REQUEST" });
+      let config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      let dataURL = `/super_admin/manage_restaurant/update_status`;
+      let response = await axios.post(dataURL, value, config);
+      dispatch({ type: "ENABLE_RESTAURANT_SUCCESS", payload: response.data });
+      dispatch(setAlert(response.data.message, "success"));
+      const token = localStorage.getItem("access_token");
+      if (token) axios.defaults.headers.common = { "x-access-token": token };
+      history.push("/manage_restaurant");
+    } catch (error) {
+      dispatch({ type: "ENABLE_RESTAURANT_FAILURE", payload: error });
+      if (error.response) {
+        dispatch(setAlert("status not changed!", "danger"));
+      } else {
+        dispatch(setAlert("Something Went wrong!", "danger"));
       }
     }
   };
+};
+
+export const showUpdateRestaurantModal = (value) => {
+  return async (dispatch) => {
+    try {
+      await dispatch({ type: "SHOW_UPDATERESTAURANT_MODAL", payload: value });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const showAddRestaurantModal = (value) => {
+  return async (dispatch) => {
+    try {
+      await dispatch({ type: "SHOW_UPDATERESTAURANT_MODAL", payload: value });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const showDeleteRestaurantModal = (value) => {
+  return async (dispatch) => {
+    try {
+      await dispatch({ type: "SHOW_DELETERESTAURANT_MODAL", payload: value });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
--- src/redux/reducers/manageRestaurantReducer.js
+++ src/redux/reducers/manageRestaurantReducer.js
@@ -1,94 +1,104 @@
-
-
 const initialState = {
-    isLoading : false,
-    errorMessage:'',
-    restaurant_Data:{},
-    selectedRestaurant:{},
-    totalrows:0,
+  isLoading: false,
+  errorMessage: "",
+  restaurant_Data: {},
+  selectedRestaurant: {},
+  totalrows: 0,
 
-    showAddRestaurantModalData:false,
-    showUpdateRestaurantModalData:false,
-    showDeleteRestaurantModalData:false,
-    };
-    
-    const manageRestaurantReducer = (state = initialState, { type, payload }) => {
-      switch (type) {
-        
-        case "GET_ALLRESTAURANT_REQUEST":
-        case "ADD_RESTAURANT_REQUEST":
-        case "GET_SELECTEDCATEGORY_REQUEST":
-        case "UPDATE_RESTAURANT_REQUEST":
-        case "DELETE_RESTAURANT_REQUEST":
-            return {
-                ...state,
-                isLoading :true,
-            };
-        case "GET_ALLRESTAURANT_SUCCESS":
-            return {
-                ...state,
-                isLoading:false,
-            restaurant_Data:payload,
-            totalrows:payload.totalRecord,
-    
-            };
-        case "ADD_RESTAURANT_SUCCESS":
-            return{
-                ...state,
-                isLoading:false,
-            };
-        case "GET_SELECTEDCATEGORY_SUCCESS":            
-            return{
-                ...state,
-                isLoading:false,
-                selectedRestaurant: payload.data
-            }  
-      
-        case "UPDATE_RESTAURANT_SUCCESS":            
-            return{
-                ...state,
-                isLoading:false
-            }   
+  showAddRestaurantModalData: false,
+  showUpdateRestaurantModalData: false,
+  showDeleteRestaurantModalData: false,
+  activeRestaurant: null,
+};
 
-        case "DELETE_RESTAURANT_SUCCESS":            
-            return{
-                ...state,
-                isLoading:false
-            } 
-             
-        case "GET_ALLRESTAURANT_FAILURE":
-        case "ADD_RESTAURANT_FAILURE":
-        case "GET_SELECTEDCATEGORY_FAILURE":  
-        case "UPDATE_RESTAURANT_FAILURE":  
-        case "DELETE_RESTAURANT_FAILURE": 
-            return {
-                ...state,
-                isLoading:false,
-                errorMessage:payload
-            };
+const manageRestaurantReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "GET_ALLRESTAURANT_REQUEST":
+    case "ADD_RESTAURANT_REQUEST":
+    case "GET_SELECTEDCATEGORY_REQUEST":
+    case "UPDATE_RESTAURANT_REQUEST":
+    case "DELETE_RESTAURANT_REQUEST":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "GET_ALLRESTAURANT_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        restaurant_Data: payload,
+        totalrows: payload.totalRecord,
+      };
+    case "ADD_RESTAURANT_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+      };
+    case "GET_SELECTEDCATEGORY_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        selectedRestaurant: payload.data,
+      };
 
-        case 'SHOW_ADDRESTAURANT_MODAL':
-            return  { 
-              ...state, 
-              showAddRestaurantModalData:payload
-            }; 
-        case 'SHOW_UPDATERESTAURANT_MODAL':
-            return  { 
-              ...state, 
-              showUpdateRestaurantModalData:payload
-            };    
-        case 'SHOW_DELETERESTAURANT_MODAL':
-            return  { 
-              ...state, 
-              showDeleteRestaurantModalData:payload
-            };        
-  
-    
+    case "UPDATE_RESTAURANT_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+      };
 
-        default:
-          return state;
-      }
-    };
-    
-    export default manageRestaurantReducer;
-    
+    case "DELETE_RESTAURANT_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+      };
+
+    case "GET_ALLRESTAURANT_FAILURE":
+    case "ADD_RESTAURANT_FAILURE":
+    case "GET_SELECTEDCATEGORY_FAILURE":
+    case "UPDATE_RESTAURANT_FAILURE":
+    case "DELETE_RESTAURANT_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        errorMessage: payload,
+      };
+
+    case "SHOW_ADDRESTAURANT_MODAL":
+      return {
+        ...state,
+        showAddRestaurantModalData: payload,
+      };
+    case "ENABLE_RESTAURANT_REQUEST":
+      return {
+        ...state,
+        activeRestaurant: payload,
+      };
+    case "ENABLE_RESTAURANT_SUCCESS":
+      return {
+        ...state,
+        activeRestaurant: payload,
+      };
+    case "ENABLE_RESTAURANT_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        activeRestaurant: payload,
+      };
+    case "SHOW_UPDATERESTAURANT_MODAL":
+      return {
+        ...state,
+        showUpdateRestaurantModalData: payload,
+      };
+    case "SHOW_DELETERESTAURANT_MODAL":
+      return {
+        ...state,
+        showDeleteRestaurantModalData: payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default manageRestaurantReducer;
